5659a306d72ca0bd57265cbc60c5d4028c3d6f4c
caiocavalcantepsc <caiop6878@gmail.com>
2026-01-15 20:57:41 -0300

    mudei pra URI o jeito que tava antes

--- estudo/__init__.py
+++ estudo/__init__.py
@@ -9,7 +9,7 @@ import os
 load_dotenv('.env')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
